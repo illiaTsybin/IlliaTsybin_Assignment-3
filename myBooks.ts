db8cf26aecbe7d4476120895f20efeddf16331a2
illiaTsybin <50668831+illiaTsybin@users.noreply.github.com>
2019-07-29 17:58:13 -0400

    Add files via upload

---
+++ myBooks.ts
@@ -0,0 +1,11 @@
+import { BooksInfo } from "../src/app/bookInfo"
+
+
+export const myBooks: BooksInfo[] = 
+[  
+   {bname:"1",bauthor:"2",bgenre:"3",byear:"4",bpic:"" },
+   {bname:"1",bauthor:"2",bgenre:"3",byear:"4",bpic:"" } , 
+   {bname:"1",bauthor:"2",bgenre:"3",byear:"4",bpic:"" },
+   {bname:"1",bauthor:"2",bgenre:"3",byear:"4",bpic:"" }   
+]
+
